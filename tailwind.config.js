8205d6cc572ea8287c1f02580719e84e24531992
ArbjonBardhaj <bardhajarbion@gmail.com>
2025-05-03 21:47:08 +0200

    Upgrade Node.js version and add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Adjust paths based on your project structure
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
