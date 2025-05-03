e5c9c07717665b25838e632399200a96467c10ce
ArbjonBardhaj <bardhajarbion@gmail.com>
2025-05-03 18:34:42 +0200

    test commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ export default function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
             <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Sign In</h2>
-            
+             <h1>this is a test</h1>
             <form className="space-y-4">
                 <div>
                     <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
